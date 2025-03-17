66ca454107bc90f1140557930c036c2364a382ae
Jakub Jareš <me@jakubjares.com>
2025-03-17 12:25:25 +0100

    vstest task

---
+++ TestProject2/Test1.cs
@@ -0,0 +1,21 @@
+﻿using BrokenLid;
+
+namespace TestProject2
+{
+    [TestClass]
+    public sealed class Test1
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+            Assert.AreEqual(1, 2);
+        }
+
+        [TestMethod]
+        public void TestMethod2()
+        {
+            new BrokenClass().SomeChildCodeIsBroken();
+
+        }
+    }
+}
